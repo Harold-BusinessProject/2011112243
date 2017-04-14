b40a07b90ad175f9e87f0b7724171a05308bcea4
Harold Cano <harold_cara_07@hotmailcom>
2017-04-14 17:25:37 -0500

    guardar?

---
+++ 2011112243/2011112243/Venta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2011112243
+{
+   public class Venta
+    {
+        public String idVenta { get; set; }
+        public decimal Costo { get; set; }
+        public Administrativo _Administrativo;
+        public Servicio _Servicio;
+        public Cliente _Cliente;
+        public TipoPago _TipoPago;
+        public TipoComprobante _tipoComprobante;
+         public Venta (Servicio Servicio, Administrativo Administrativo, Cliente Cliente, TipoPago TipoPago, TipoComprobante  )
+
+       
+
+    }
+}
